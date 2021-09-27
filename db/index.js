7fd2fa5f5f21c19356f41b11202102b669ed4efc
BBrownley <brendobrownley@yahoo.com>
2021-09-26 22:00:01 -0400

    fixing deployment bug - was not pointing to db pw

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ const mysql = require("mysql");
 const connection = mysql.createPool({
   connectionLimit: 1000,
   host: process.env.PRODUCTION_DB_HOST || "localhost",
-  password: process.env.PRODUCTION_DB_USER || "",
+  password: process.env.PRODUCTION_DB_PASSWORD || "",
   user: process.env.PRODUCTION_DB_USER || "root",
   database: process.env.heroku_61b8f24467ac49f || "reddit-clone"
 });
